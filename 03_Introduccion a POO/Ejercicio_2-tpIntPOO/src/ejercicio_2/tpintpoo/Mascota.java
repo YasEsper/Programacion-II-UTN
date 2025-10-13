f264cdaaa97fb795eec41c1c2cb91a06a14151f8
Yasesper <yasesper85@gmail.com>
2025-10-13 11:57:20 -0300

    Ejercicios Resuelto tp 2 - Progrmacion Estrucutrada

---
+++ 03_Introduccion a POO/Ejercicio_2-tpIntPOO/src/ejercicio_2/tpintpoo/Mascota.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package ejercicio_2.tpintpoo;
+
+public class Mascota {
+
+    public static void main(String[] args) {
+        Mascota m = new Mascota();
+
+        m.nombre = "Luna";
+        m.especie = "Gata";
+        m.edad = 3;
+
+        m.mostrarInformacion();
+        m.cumplirAnios();
+        m.mostrarInformacion();
+
+    }
+
+}
